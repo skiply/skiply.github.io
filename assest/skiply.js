4c723712a5ae277a4f582b28b8aee9b326741078
vikas5914 <vikas@kapadiya.net>
2014-11-21 10:55:27 +0530

    third release

--- assest/skiply.js
+++ assest/skiply.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
 if ($('#url').val() == '') {
-    var defaultValue3 = 'http://adf.ly/uCCdU' + '\n' + 'http://j.gs/4lQt' + '\n' + 'http://q.gs/7oUUo' + '\n' + 'http://ay.gy/uCCdU' + '\n' + 'http://adfoc.us/x50624552' + '\n' +'http://sh.st/uXXin' + '\n' + 'http://linkshrink.net/79nmLJ' + '\n' + 'http://www.oni.vn/2VTDb' ;
+    var defaultValue3 = 'http://adf.ly/uCCdU' + '\n' + 'http://j.gs/4lQt' + '\n' + 'http://q.gs/7oUUo' + '\n' + 'http://ay.gy/uCCdU' + '\n' + 'http://adfoc.us/x50624552' + '\n' +'http://sh.st/uXXin' + '\n' + 'http://linkshrink.net/79nmLJ' + '\n' + 'http://www.oni.vn/2VTDb' + '\n' + 'http://adlock.org/58WqwS' + '\n' + 'http://cur.lv/fr436' + '\n'+ 'http://goo.gl/x8Ubfb' + '\n' + 'http://is.gd/uCA0yW' + '\n' + 'http://bit.ly/1yY5n2b' + '\n' +'http://tinyurl.com/puvz4lp' + '\n' + 'https://safelinking.net/d/c488eddf88' ;
     $('#url').val(defaultValue3);
 }
 $('form').submit(function(e) {
